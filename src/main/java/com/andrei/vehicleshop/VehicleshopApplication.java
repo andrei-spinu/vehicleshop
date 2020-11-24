e37cd77606c453eba524943f903c63bc2a0ab91e
Spinu Andrei <aandreispinu@gmail.com>
2020-11-24 15:27:46 +0200

    Initial commit

---
+++ src/main/java/com/andrei/vehicleshop/VehicleshopApplication.java
@@ -0,0 +1,13 @@
+package com.andrei.vehicleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrei/vehicleshop/VehicleshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrei.vehicleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
